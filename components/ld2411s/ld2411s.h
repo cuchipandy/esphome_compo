0db5db1c7768cba596ace456fde3f180a9ec4892
cuchipandy <110701228+cuchipandy@users.noreply.github.com>
2025-02-20 13:43:28 +0100

    Update ld2411s.h

--- components/ld2411s/ld2411s.h
+++ components/ld2411s/ld2411s.h
@@ -1,130 +1,80 @@
+#pragma once
+
 #include "esphome.h"
 
-using namespace esphome;
+namespace esphome {
+namespace ld2411s {
 
-class UARTSensor : public Component, public UARTDevice {
-  public:
-  UARTSensor(UARTComponent *parent) : UARTDevice(parent) {}
-  Sensor* presence_sensor = new Sensor();
-  Sensor* motion_sensor = new Sensor();
-  Sensor* distance_sensor = new Sensor();
-  Sensor* max_motion_sensor = new Sensor();
-  Sensor* min_motion_sensor = new Sensor();
-  Sensor* max_presence_sensor = new Sensor();
-  Sensor* min_presence_sensor = new Sensor();
-  Sensor* unocc_time_sensor = new Sensor();
+class LD2411SSensor : public Component, public UARTDevice {
+ public:
+  LD2411SSensor(UARTComponent *parent) : UARTDevice(parent) {}
 
-  void setup() {
-  }
+  Sensor *presence_sensor{new Sensor()};
+  Sensor *motion_sensor{new Sensor()};
+  Sensor *distance_sensor{new Sensor()};
+  Sensor *max_motion_sensor{new Sensor()};
+  Sensor *min_motion_sensor{new Sensor()};
+  Sensor *max_presence_sensor{new Sensor()};
+  Sensor *min_presence_sensor{new Sensor()};
+  Sensor *unocc_time_sensor{new Sensor()};
 
-  std::vector<int> bytes;
+  void setup() override {}
 
-void loop() override
-{
-    while (available())
-    {
-        bytes.push_back(read());
+  void loop() override {
+    while (available()) {
+      uint8_t byte = read();
+      this->bytes_.push_back(byte);
 
-        //End of Frame is 0x55 0x55
-        if( (bytes[bytes.size()-2] == 0x55 && bytes[bytes.size()-1] == 0x55) || ( bytes[bytes.size()-4] == 0x04 && bytes[bytes.size()-3] == 0x03 && bytes[bytes.size()-2] == 0x02 && bytes[bytes.size()-1] == 0x01 ) )
-        {            
-						processPacket();
-            bytes.clear();
-        }
+      if (this->bytes_.size() >= 2 &&
+          ((this->bytes_[this->bytes_.size() - 2] == 0x55 && this->bytes_[this->bytes_.size() - 1] == 0x55) ||
+           (this->bytes_.size() >= 4 && this->bytes_[this->bytes_.size() - 4] == 0x04 &&
+            this->bytes_[this->bytes_.size() - 3] == 0x03 && this->bytes_[this->bytes_.size() - 2] == 0x02 &&
+            this->bytes_[this->bytes_.size() - 1] == 0x01))) {
+        this->process_packet_();
+        this->bytes_.clear();
+      }
     }
-}
-
-  void processPacket() {            
-
-    std::string str = "";
-	
-	if ((bytes[0] == 0xAA) && (bytes[1] == 0xAA) && (bytes[2] == 0x00)) { 
-		
-		presence_sensor->publish_state(0);
-		motion_sensor->publish_state(0);
-		
-		return;
-	}
-
-  if ( (bytes[0] == 0xAA) && (bytes[1] == 0xAA) && (bytes[2] == 0x01) ) {        
-      
-		presence_sensor->publish_state(1);
-		motion_sensor->publish_state(1);
-
-    unsigned char byte3 = bytes[3];
-    unsigned char byte4 = bytes[4];
-    
-		unsigned int distanceHex = (byte4 << 8) | byte3;
-    
-		int distanceCm = static_cast<int>(distanceHex);
-		
-		distance_sensor->publish_state(distanceCm);
-		return;
-	}
-
-  if ( (bytes[0] == 0xAA) && (bytes[1] == 0xAA) && (bytes[2] == 0x02) ) {       
-      
-		presence_sensor->publish_state(1);
-		motion_sensor->publish_state(0);
-
-    unsigned char byte3 = bytes[3];
-    unsigned char byte4 = bytes[4];
-    
-		unsigned int distanceHex = (byte4 << 8) | byte3;
-    
-		int distanceCm = static_cast<int>(distanceHex);
-		
-		distance_sensor->publish_state(distanceCm);
-
-		return;
-	}
-
-  if ( (bytes[0] == 0xFD) && (bytes[1] == 0xFC) && (bytes[2] == 0xFB) && (bytes[3] == 0xFA) && (bytes[6] == 0x73) && (bytes[7] == 0x01) ) {
-		
-    unsigned char maxmlow = bytes[10];
-    unsigned char maxmhigh = bytes[11];
-
-    unsigned int maxmhex = (maxmhigh << 8) | maxmlow;
-
-    int max_m = static_cast<int>(maxmhex);
-
-    unsigned char minmlow = bytes[12];
-    unsigned char minmhigh = bytes[13];
-
-    unsigned int minmhex = (minmhigh << 8) | minmlow;
-
-    int min_m = static_cast<int>(minmhex);
-
-    unsigned char maxplow = bytes[14];
-    unsigned char maxphigh = bytes[15];
-
-    unsigned int maxphex = (maxphigh << 8) | maxplow;
-
-    int max_p = static_cast<int>(maxphex);
-
-    unsigned char minplow = bytes[16];
-    unsigned char minphigh = bytes[17];
-
-    unsigned int minphex = (minphigh << 8) | minplow;
-
-    int min_p = static_cast<int>(minphex);
-
-    unsigned char unocclow = bytes[18];
-    unsigned char unocchigh = bytes[19];
-
-    unsigned int unocchex = (unocchigh << 8) | unocclow;
-
-    int unocc_time = static_cast<int>(unocchex);
-
-    max_motion_sensor->publish_state(max_m);
-		min_motion_sensor->publish_state(min_m);
-		max_presence_sensor->publish_state(max_p);
-		min_presence_sensor->publish_state(min_p);
-		unocc_time_sensor->publish_state(unocc_time / 10);
-
-		return;
-	}
-
   }
 
+ protected:
+  void process_packet_() {
+    if (this->bytes_.size() < 3)
+      return;
+
+    if (this->bytes_[0] == 0xAA && this->bytes_[1] == 0xAA) {
+      switch (this->bytes_[2]) {
+        case 0x00:
+          this->presence_sensor->publish_state(0);
+          this->motion_sensor->publish_state(0);
+          break;
+        case 0x01:
+        case 0x02:
+          this->presence_sensor->publish_state(1);
+          this->motion_sensor->publish_state(this->bytes_[2] == 0x01);
+          if (this->bytes_.size() >= 5) {
+            uint16_t distance = (this->bytes_[4] << 8) | this->bytes_[3];
+            this->distance_sensor->publish_state(distance);
+          }
+          break;
+      }
+    } else if (this->bytes_.size() >= 20 && this->bytes_[0] == 0xFD && this->bytes_[1] == 0xFC &&
+               this->bytes_[2] == 0xFB && this->bytes_[3] == 0xFA && this->bytes_[6] == 0x73 && this->bytes_[7] == 0x01) {
+      uint16_t max_motion = (this->bytes_[11] << 8) | this->bytes_[10];
+      uint16_t min_motion = (this->bytes_[13] << 8) | this->bytes_[12];
+      uint16_t max_presence = (this->bytes_[15] << 8) | this->bytes_[14];
+      uint16_t min_presence = (this->bytes_[17] << 8) | this->bytes_[16];
+      uint16_t unocc_time = (this->bytes_[19] << 8) | this->bytes_[18];
+
+      this->max_motion_sensor->publish_state(max_motion);
+      this->min_motion_sensor->publish_state(min_motion);
+      this->max_presence_sensor->publish_state(max_presence);
+      this->min_presence_sensor->publish_state(min_presence);
+      this->unocc_time_sensor->publish_state(unocc_time / 10.0f);
+    }
+  }
+
+  std::vector<uint8_t> bytes_;
 };
+
+}  // namespace ld2411s
+}  // namespace esphome
